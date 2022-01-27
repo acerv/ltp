71184ae109fed080136bc7c3e49f826f8a02aa2b
Andrea Cervesato <andrea.cervesato@suse.de>
2022-01-27 17:45:49 +0100

    Refactoring setpgid03.c test using new LTP API
    Removed TST_CHECKPOINT_INIT and replaced it with the .needs_checkpoints
    LTP test feature. Simplified source code.

--- testcases/kernel/syscalls/setpgid/setpgid03_child.c
+++ testcases/kernel/syscalls/setpgid/setpgid03_child.c
@@ -1,32 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2013 Oracle and/or its affiliates. All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write the Free Software Foundation,
- * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Copyright (C) 2021 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
 
-#include "test.h"
+#include "tst_test.h"
 
-char *TCID = "setpgid03_child";
-
-
-int main(void)
+static void run(void)
 {
-	TST_CHECKPOINT_INIT(NULL);
-
-	TST_SAFE_CHECKPOINT_WAKE(NULL, 0);
-	TST_SAFE_CHECKPOINT_WAIT(NULL, 0);
-
-	return 0;
+	TST_CHECKPOINT_WAKE_AND_WAIT(0);
 }
+
+static struct tst_test test = {
+	.test_all = run,
+	.needs_checkpoints = 1,
+};
