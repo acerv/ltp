8fd74b4e1021b6722991e0e0fe7a36d6706210e5
Andrea Cervesato <andrea.cervesato@suse.com>
2024-03-20 11:05:28 +0100

    Introduce Doxygen support inside documentation
    With Doxygen support inside documentation, we are able to generate
    a full API documentation from LTP headers. The idea is that we don't
    need to keep track of both source code and API documentation files,
    but we will auto-generate any API documentation via Doxygen + Sphinx.

--- doc_new/conf.py
+++ doc_new/conf.py
@@ -3,6 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import subprocess
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,8 +17,10 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+# Doxygen support
+extensions = ['breathe']
+breathe_default_project = "ltp"
+breathe_projects = {'ltp': 'xml'}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -23,5 +28,14 @@ extensions = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+def generate_doxygen_xml(app):
+    """
+    Generate API documentation via Doxygen if we are not
+    inside a readthedocs server.
+    """
+    if not os.environ.get('READTHEDOCS', None):
+        subprocess.call('doxygen Doxygen.in', shell=True)
+
 def setup(app):
     app.add_css_file('custom.css')
+    app.connect("builder-inited", generate_doxygen_xml)
--- doc_new/developers/api_c_tests.rst
+++ doc_new/developers/api_c_tests.rst
@@ -1,4 +1,7 @@
 .. SPDX-License-Identifier: GPL-2.0-or-later
 
+.. Include them in this file with:
+.. .. doxygenfile:: tst_uid.h
+
 Developing using C API
 ======================
