ed71749164c476e67b5d5e8ee223327a07065778
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:14 +0200

    syscalls/sched_setattr01: Convert to new API
    Convert sched_setattr01 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/sched_setattr/sched_setattr01.c
+++ testcases/kernel/syscalls/sched_setattr/sched_setattr01.c
@@ -1,40 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) Huawei Technologies Co., Ltd., 2015
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- * the GNU General Public License for more details.
+ * Copyright (c) Linux Test Project, 2015-2026
  */
- /* Description:
- *   Verify that:
- *              1) sched_setattr succeed with correct parameters
- *              2) sched_setattr fails with unused pid
- *              3) sched_setattr fails with invalid address
- *              4) sched_setattr fails with invalid flag
+
+/*\
+ * Verify that :manpage:`sched_setattr(2)` succeeds with valid parameters
+ * and fails with the correct errno for invalid inputs.
+ *
+ * - ESRCH when pid does not exist
+ * - EINVAL when attr address is NULL
+ * - EINVAL when flags value is invalid
  */
 
 #define _GNU_SOURCE
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <linux/unistd.h>
-#include <linux/kernel.h>
-#include <linux/types.h>
-#include <sys/syscall.h>
-#include <pthread.h>
+
 #include <errno.h>
-
-#include "test.h"
+#include "tst_test.h"
 #include "lapi/sched.h"
-
-char *TCID = "sched_setattr01";
 
 #define RUNTIME_VAL 10000000
 #define PERIOD_VAL 30000000
@@ -48,87 +31,50 @@ static struct sched_attr attr = {
 	.sched_flags = 0,
 	.sched_nice = 0,
 	.sched_priority = 0,
-
 	.sched_policy = SCHED_DEADLINE,
 	.sched_runtime = RUNTIME_VAL,
 	.sched_period = PERIOD_VAL,
 	.sched_deadline = DEADLINE_VAL,
 };
 
-static struct test_case {
+static struct tcase {
 	pid_t *pid;
 	struct sched_attr *a;
 	unsigned int flags;
-	int exp_return;
 	int exp_errno;
-} test_cases[] = {
-	{&pid, &attr, 0, 0, 0},
-	{&unused_pid, &attr, 0, -1, ESRCH},
-	{&pid, NULL, 0, -1, EINVAL},
-	{&pid, &attr, 1000, -1, EINVAL}
+} tcases[] = {
+	{&pid, &attr, 0, 0},
+	{&unused_pid, &attr, 0, ESRCH},
+	{&pid, NULL, 0, EINVAL},
+	{&pid, &attr, 1000, EINVAL},
 };
 
-static void setup(void);
-static void sched_setattr_verify(const struct test_case *test);
-
-int TST_TOTAL = ARRAY_SIZE(test_cases);
-
-void *do_test(void *data LTP_ATTRIBUTE_UNUSED)
+static void verify_sched_setattr(unsigned int n)
 {
-	int i;
+	struct tcase *tc = &tcases[n];
 
-	for (i = 0; i < TST_TOTAL; i++)
-		sched_setattr_verify(&test_cases[i]);
-
-	return NULL;
-}
-
-static void sched_setattr_verify(const struct test_case *test)
-{
-	TEST(sched_setattr(*(test->pid), test->a, test->flags));
-
-	if (TEST_RETURN != test->exp_return) {
-		tst_resm(TFAIL | TTERRNO, "sched_setattr(%i,attr,%u) "
-		         "returned: %ld expected: %d",
-		         *(test->pid), test->flags,
-		         TEST_RETURN, test->exp_return);
-		return;
+	if (tc->exp_errno) {
+		TST_EXP_FAIL(sched_setattr(*tc->pid, tc->a, tc->flags),
+				tc->exp_errno,
+				"sched_setattr(%d, %s, %u)",
+				*tc->pid,
+				tc->a ? "attr" : "NULL",
+				tc->flags);
+	} else {
+		TST_EXP_PASS(sched_setattr(*tc->pid, tc->a, tc->flags),
+				"sched_setattr(%d, attr, %u)",
+				*tc->pid, tc->flags);
 	}
-
-	if (TEST_ERRNO == test->exp_errno) {
-		tst_resm(TPASS | TTERRNO,
-			"sched_setattr() works as expected");
-		return;
-	}
-
-	tst_resm(TFAIL | TTERRNO, "sched_setattr(%i,attr,%u): "
-		"expected: %d - %s",
-		*(test->pid), test->flags,
-		test->exp_errno, tst_strerrno(test->exp_errno));
 }
 
-int main(int argc, char **argv)
+static void setup(void)
 {
-	pthread_t thread;
-	int lc;
-
-	tst_parse_opts(argc, argv, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		pthread_create(&thread, NULL, do_test, NULL);
-		pthread_join(thread, NULL);
-	}
-
-	tst_exit();
+	unused_pid = tst_get_unused_pid();
 }
 
-void setup(void)
-{
-	unused_pid = tst_get_unused_pid(setup);
-
-	tst_require_root();
-
-	TEST_PAUSE;
-}
+static struct tst_test test = {
+	.setup = setup,
+	.test = verify_sched_setattr,
+	.tcnt = ARRAY_SIZE(tcases),
+	.needs_root = 1,
+};
