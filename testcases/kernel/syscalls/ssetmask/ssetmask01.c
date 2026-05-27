bf660b044514b0ea2fbdfd20c04ac75834937a1d
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:14 +0200

    syscalls/ssetmask01: Convert to new API
    Convert ssetmask01 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/ssetmask/ssetmask01.c
+++ testcases/kernel/syscalls/ssetmask/ssetmask01.c
@@ -1,131 +1,35 @@
-/******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007                                   */
-/*                                                                            */
-/* This program is free software;  you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or          */
-/* (at your option) any later version.                                        */
-/*                                                                            */
-/* This program is distributed in the hope that it will be useful,            */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  */
-/* the GNU General Public License for more details.                           */
-/*                                                                            */
-/* You should have received a copy of the GNU General Public License          */
-/* along with this program;  if not, write to the Free Software               */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
-/*                                                                            */
-/******************************************************************************/
-/******************************************************************************/
-/*                                                                            */
-/* File:        ssetmask01.c                                           */
-/*                                                                            */
-/* Description: This tests the ssetmask() syscall                      */
-/*                                                                            */
-/* Usage:  <for command-line>                                                 */
-/* ssetmask01 [-c n] [-e][-i n] [-I x] [-p x] [-t]                     */
-/*      where,  -c n : Run n copies concurrently.                             */
-/*              -e   : Turn on errno logging.                                 */
-/*              -i n : Execute test n times.                                  */
-/*              -I x : Execute test for x seconds.                            */
-/*              -P x : Pause for x seconds between iterations.                */
-/*              -t   : Turn on syscall timing.                                */
-/*                                                                            */
-/* Total Tests: 2                                                             */
-/*                                                                            */
-/* Test Name:   ssetmask01                                             */
-/* History:     Porting from Crackerjack to LTP is done by                    */
-/*              Manas Kumar Nayak maknayak@in.ibm.com>                        */
-/******************************************************************************/
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) Crackerjack Project, 2007
+ * Copyright (c) Manas Kumar Nayak <maknayak@in.ibm.com>
+ */
 
-#include <stdio.h>
-#include <errno.h>
-#include <stdlib.h>
+/*\
+ * Verify that :manpage:`ssetmask(2)` correctly sets the current signal mask
+ * and returns the previous one.
+ *
+ * - Set the signal mask to SIGALRM using ssetmask() and verify via
+ *   sgetmask() that the mask is SIGALRM
+ * - Set the signal mask to SIGUSR1 using ssetmask() and verify that
+ *   the returned previous mask is SIGALRM
+ */
+
 #include <signal.h>
 
-#include "test.h"
+#include "tst_test.h"
 #include "lapi/syscalls.h"
 
-char *TCID = "ssetmask01";
-int testno;
-int TST_TOTAL = 2;
-
-/* Extern Global Functions */
-/******************************************************************************/
-/*                                                                            */
-/* Function:    cleanup                                                       */
-/*                                                                            */
-/* Description: Performs all one time clean up for this test on successful    */
-/*              completion,  premature exit or  failure. Closes all temporary */
-/*              files, removes all temporary directories exits the test with  */
-/*              appropriate return code by calling tst_exit() function.       */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits calling tst_exit(). Non '0' return code.   */
-/*              On success - Exits calling tst_exit(). With '0' return code.  */
-/*                                                                            */
-/******************************************************************************/
-void cleanup(void)
+static void run(void)
 {
+	tst_syscall(__NR_ssetmask, SIGALRM);
 
-	tst_rmdir();
+	TST_EXP_VAL(tst_syscall(__NR_sgetmask), SIGALRM,
+			"sgetmask() after ssetmask(SIGALRM)");
 
+	TST_EXP_VAL(tst_syscall(__NR_ssetmask, SIGUSR1), SIGALRM,
+			"ssetmask(SIGUSR1) returns previous mask");
 }
 
-/* Local  Functions */
-/******************************************************************************/
-/*                                                                            */
-/* Function:    setup                                                         */
-/*                                                                            */
-/* Description: Performs all one time setup for this test. This function is   */
-/*              typically used to capture signals, create temporary dirs      */
-/*              and temporary files that may be used in the course of this    */
-/*              test.                                                         */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits by calling cleanup().                      */
-/*              On success - returns 0.                                       */
-/*                                                                            */
-/******************************************************************************/
-void setup(void)
-{
-	/* Capture signals if any */
-	/* Create temporary directories */
-	TEST_PAUSE;
-	tst_tmpdir();
-}
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); ++lc) {
-		tst_count = 0;
-		for (testno = 0; testno < TST_TOTAL; ++testno) {
-			tst_syscall(__NR_ssetmask, SIGALRM);
-			TEST(tst_syscall(__NR_sgetmask));
-			if (TEST_RETURN != SIGALRM) {
-				tst_brkm(TFAIL | TTERRNO, cleanup,
-					 "sgetmask() failed");
-			}
-			TEST(tst_syscall(__NR_ssetmask, SIGUSR1));
-			if (TEST_RETURN != SIGALRM) {
-				tst_brkm(TFAIL | TTERRNO, cleanup,
-					 "ssetmask() failed");
-			}
-			tst_resm(TPASS, "Got SIGALRM--Test PASS ");
-		}
-	}
-	cleanup();
-	tst_exit();
-}
+static struct tst_test test = {
+	.test_all = run,
+};
