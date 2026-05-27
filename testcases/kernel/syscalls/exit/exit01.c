21ff28408685ab46ae878e361154801de1aebe92
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:13 +0200

    syscalls/exit01: Convert to new API
    Convert exit01 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/exit/exit01.c
+++ testcases/kernel/syscalls/exit/exit01.c
@@ -1,121 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) International Business Machines Corp., 2001
  * Copyright (c) 2012 Cyril Hrubis <chrubis@suse.cz>
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-/*
- * Check that exit returns the correct values to the waiting parent
+/*\
+ * Verify that :manpage:`exit(2)` returns the correct status to the
+ * waiting parent. The child exits with a known value and the parent
+ * checks that wait() reports the expected exit code and no signal.
  */
 
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <signal.h>
-#include <errno.h>
-#include "test.h"
+#include "tst_test.h"
 
-static void cleanup(void);
-static void setup(void);
-
-char *TCID = "exit01";
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
+static void run(void)
 {
-	int pid, npid, sig, nsig, exno, nexno, status;
-	int rval = 0;
-	int lc;
+	int status;
+	pid_t pid;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	pid = SAFE_FORK();
+	if (!pid)
+		exit(1);
 
-	setup();
+	SAFE_WAITPID(pid, &status, 0);
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		sig = 0;
-		exno = 1;
-
-		pid = tst_fork();
-
-		switch (pid) {
-		case 0:
-			exit(exno);
-		break;
-		case -1:
-			tst_brkm(TBROK | TERRNO, cleanup, "fork() failed");
-		break;
-		default:
-			npid = wait(&status);
-
-			if (npid != pid) {
-				tst_resm(TFAIL, "wait error: "
-					 "unexpected pid returned");
-				rval = 1;
-			}
-
-			nsig = status % 256;
-
-			/*
-			 * Check if the core dump bit has been set, bit # 7
-			 */
-			if (nsig >= 128) {
-				nsig = nsig - 128;
-			}
-
-			/*
-			 * nsig is the signal number returned by wait
-			 */
-			if (nsig != sig) {
-				tst_resm(TFAIL, "wait error: "
-					 "unexpected signal returned");
-				rval = 1;
-			}
-
-			/*
-			 * nexno is the exit number returned by wait
-			 */
-			nexno = status / 256;
-			if (nexno != exno) {
-				tst_resm(TFAIL, "wait error: "
-					 "unexpected exit number returned");
-				rval = 1;
-			}
-		break;
-		}
-
-		if (rval != 1) {
-			tst_resm(TPASS, "exit() test PASSED");
-		}
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 1) {
+		tst_res(TPASS, "exit(1) returned correct status to parent");
+	} else {
+		tst_res(TFAIL, "unexpected wait status: %s",
+			tst_strstatus(status));
 	}
-	
-	cleanup();
-	tst_exit();
 }
 
-static void setup(void)
-{
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-}
-
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.forks_child = 1,
+	.test_all = run,
+};
