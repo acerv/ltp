678da40ede3685fbc2249bb3528035ba2ca84a5b
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:14 +0200

    syscalls/mprotect03: Convert to new API
    Convert mprotect03 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/mprotect/mprotect03.c
+++ testcases/kernel/syscalls/mprotect/mprotect03.c
@@ -1,155 +1,59 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) International Business Machines  Corp., 2001
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines Corp., 2001
+ * Ported by Wayne Boyer
  */
 
-/*
- * NAME
- *	mprotect03.c
- *
- * DESCRIPTION
- *	Testcase to check the mprotect(2) system call.
- *
- * ALGORITHM
- *	Create a shared mapped file region with PROT_READ | PROT_WRITE
- *	using the mmap(2) call. Then, use mprotect(2) to disable the
- *	write permission on the mapped region. Then, attempt to write to
- *	the mapped region using memcpy(). This would generate a sigsegv.
- *	Since the sigsegv is generated, this needs to be done in a child
- *	process (as sigsegv would repeatedly be generated). The testcase
- *	succeeds only when this sigsegv is generated while attempting to
- *	memcpy() on a shared region with only read permission.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
- *      05/2002 changed over to use tst_sig instead of sigaction
+/*\
+ * Verify that :manpage:`mprotect(2)` correctly removes write permission
+ * from a shared mapped region. A child process attempts to write to
+ * the read-only protected region and is expected to receive SIGSEGV.
  */
 
-#include <errno.h>
-#include <fcntl.h>
 #include <sys/mman.h>
-#include <limits.h>
-#include <signal.h>
 #include <sys/wait.h>
-#include "test.h"
 
-#include "tso_safe_macros.h"
+#include "tst_test.h"
 
-#ifndef PAGESIZE
-#define PAGESIZE 4096
-#endif
-#define FAILED 1
-
-static void cleanup(void);
-static void setup(void);
-
-char *TCID = "mprotect03";
-int TST_TOTAL = 1;
-int status;
-char file1[BUFSIZ];
-
-int main(int ac, char **av)
+static void run(void)
 {
-	int lc;
-
 	char *addr;
-	int fd, pid;
+	int fd, status;
 	char *buf = "abcdefghijklmnopqrstuvwxyz";
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	fd = SAFE_OPEN("testfile", O_RDWR | O_CREAT, 0777);
+	SAFE_WRITE(SAFE_WRITE_ALL, fd, buf, strlen(buf));
 
-	setup();
+	addr = SAFE_MMAP(NULL, strlen(buf), PROT_READ | PROT_WRITE,
+			MAP_SHARED, fd, 0);
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
+	TST_EXP_PASS(mprotect(addr, strlen(buf), PROT_READ));
+	if (!TST_PASS)
+		goto out;
 
-		if ((fd = open(file1, O_RDWR | O_CREAT, 0777)) < 0)
-			tst_brkm(TBROK, cleanup, "open failed");
+	pid_t pid = SAFE_FORK();
 
-		SAFE_WRITE(cleanup, SAFE_WRITE_ALL, fd, buf, strlen(buf));
-
-		/*
-		 * mmap the PAGESIZE bytes as read only.
-		 */
-		addr = mmap(0, strlen(buf), PROT_READ | PROT_WRITE, MAP_SHARED,
-			    fd, 0);
-		if (addr == MAP_FAILED)
-			tst_brkm(TBROK, cleanup, "mmap failed");
-
-		/*
-		 * Try to change the protection to WRITE.
-		 */
-		TEST(mprotect(addr, strlen(buf), PROT_READ));
-
-		if (TEST_RETURN != -1) {
-			if ((pid = tst_fork()) == -1) {
-				tst_brkm(TBROK, cleanup, "fork failed");
-			}
-
-			if (pid == 0) {
-				memcpy(addr, buf, strlen(buf));
-				tst_resm(TINFO, "memcpy() did "
-					 "not generate SIGSEGV");
-				exit(1);
-			}
-
-			waitpid(pid, &status, 0);
-			if (WEXITSTATUS(status) != 0) {
-				tst_resm(TFAIL, "child returned "
-					 "unexpected status");
-			} else {
-				tst_resm(TPASS, "SIGSEGV generated "
-					 "as expected");
-			}
-		} else {
-			tst_resm(TFAIL, "mprotect failed "
-				 "unexpectedly, errno: %d", errno);
-		}
-
-		/* clean up things in case we are looping */
-		SAFE_MUNMAP(cleanup, addr, strlen(buf));
-		SAFE_CLOSE(cleanup, fd);
-		SAFE_UNLINK(cleanup, file1);
+	if (!pid) {
+		memcpy(addr, buf, strlen(buf));
+		tst_res(TINFO, "memcpy() did not generate SIGSEGV");
+		exit(1);
 	}
 
-	cleanup();
-	tst_exit();
+	SAFE_WAITPID(pid, &status, 0);
+
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGSEGV)
+		tst_res(TPASS, "SIGSEGV generated as expected");
+	else
+		tst_res(TFAIL, "child: %s", tst_strstatus(status));
+
+out:
+	SAFE_MUNMAP(addr, strlen(buf));
+	SAFE_CLOSE(fd);
+	SAFE_UNLINK("testfile");
 }
 
-static void sighandler(int sig)
-{
-	if (sig == SIGSEGV) {
-		tst_resm(TINFO, "received signal: SIGSEGV");
-		tst_exit();
-	} else
-		tst_brkm(TBROK, 0, "Unexpected signal %d received.", sig);
-}
-
-static void setup(void)
-{
-	tst_sig(FORK, sighandler, NULL);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	sprintf(file1, "mprotect03.tmp.%d", getpid());
-}
-
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.test_all = run,
+	.forks_child = 1,
+	.needs_tmpdir = 1,
+};
