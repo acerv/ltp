be6e6dc398ee77aefb9a95851125d8e565c1237e
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:14 +0200

    syscalls/renameat202: Convert to new API
    Convert renameat202 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/renameat2/renameat202.c
+++ testcases/kernel/syscalls/renameat2/renameat202.c
@@ -1,30 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2015 Cedric Hnyda <chnyda@suse.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write the Free Software Foundation,
- * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
- /* Description:
- *   Calls renameat2(2) with the flag RENAME_EXCHANGE and check that
- *   the content was swapped
+/*\
+ * Verify that :manpage:`renameat2(2)` with RENAME_EXCHANGE swaps the
+ * contents of two files. After the call, the file that previously had
+ * content should now be empty and vice versa.
  */
 
 #define _GNU_SOURCE
 
-#include "test.h"
-#include "tso_safe_macros.h"
+#include "tst_test.h"
 #include "lapi/fcntl.h"
 #include "renameat2.h"
 
@@ -34,133 +21,83 @@
 #define TEST_FILE "test_file"
 #define TEST_FILE2 "test_file2"
 
-char *TCID = "renameat202";
-
-static int olddirfd;
-static int newdirfd;
-static int fd = -1;
-static int cnt;
+static int olddirfd = -1;
+static int newdirfd = -1;
 
 static const char content[] = "content";
-static long fs_type;
-
-
-int TST_TOTAL = 1;
-
-static void setup(void);
-static void cleanup(void);
-static void renameat2_verify(void);
-
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(renameat2(olddirfd, TEST_FILE,
-				newdirfd, TEST_FILE2, RENAME_EXCHANGE));
-
-		cnt++;
-
-		renameat2_verify();
-	}
-
-	cleanup();
-	tst_exit();
-}
 
 static void setup(void)
 {
-	tst_tmpdir();
+	SAFE_MKDIR(TEST_DIR, 0700);
+	SAFE_MKDIR(TEST_DIR2, 0700);
 
-	fs_type = tst_fs_type(cleanup, ".");
+	SAFE_TOUCH(TEST_DIR TEST_FILE, 0600, NULL);
+	SAFE_TOUCH(TEST_DIR2 TEST_FILE2, 0600, NULL);
 
-	SAFE_MKDIR(cleanup, TEST_DIR, 0700);
-	SAFE_MKDIR(cleanup, TEST_DIR2, 0700);
+	olddirfd = SAFE_OPEN(TEST_DIR, O_DIRECTORY);
+	newdirfd = SAFE_OPEN(TEST_DIR2, O_DIRECTORY);
 
-	SAFE_TOUCH(cleanup, TEST_DIR TEST_FILE, 0600, NULL);
-	SAFE_TOUCH(cleanup, TEST_DIR2 TEST_FILE2, 0600, NULL);
+	SAFE_FILE_PRINTF(TEST_DIR TEST_FILE, "%s", content);
+}
 
-	olddirfd = SAFE_OPEN(cleanup, TEST_DIR, O_DIRECTORY);
-	newdirfd = SAFE_OPEN(cleanup, TEST_DIR2, O_DIRECTORY);
+static void run(void)
+{
+	char str[BUFSIZ] = { 0 };
+	struct stat st;
+	int fd, readn;
 
-	SAFE_FILE_PRINTF(cleanup, TEST_DIR TEST_FILE, "%s", content);
+	TST_EXP_PASS(renameat2(olddirfd, TEST_FILE,
+			newdirfd, TEST_FILE2, RENAME_EXCHANGE));
+	if (!TST_PASS)
+		return;
 
+	/* After exchange: content should be in TEST_DIR2/TEST_FILE2 */
+	fd = SAFE_OPEN(TEST_DIR2 TEST_FILE2, O_RDONLY);
+	readn = SAFE_READ(0, fd, str, BUFSIZ);
+	SAFE_CLOSE(fd);
+
+	SAFE_STAT(TEST_DIR TEST_FILE, &st);
+
+	if (readn != (int)(sizeof(content) - 1)) {
+		tst_res(TFAIL, "wrong byte count: expected %zu, got %d",
+			sizeof(content) - 1, readn);
+		return;
+	}
+
+	if (strncmp(content, str, sizeof(content) - 1)) {
+		tst_res(TFAIL, "content mismatch: expected '%s', got '%s'",
+			content, str);
+		return;
+	}
+
+	if (st.st_size) {
+		tst_res(TFAIL, "swapped file has non-zero size");
+		return;
+	}
+
+	tst_res(TPASS, "RENAME_EXCHANGE swapped contents correctly");
+
+	/* Swap back for next iteration */
+	SAFE_FILE_PRINTF(TEST_DIR TEST_FILE, "%s", content);
+	SAFE_FILE_PRINTF(TEST_DIR2 TEST_FILE2, "%s", "");
 }
 
 static void cleanup(void)
 {
-	if (olddirfd > 0 && close(olddirfd) < 0)
-		tst_resm(TWARN | TERRNO, "close olddirfd failed");
+	if (olddirfd != -1)
+		SAFE_CLOSE(olddirfd);
 
-	if (newdirfd > 0 && close(newdirfd) < 0)
-		tst_resm(TWARN | TERRNO, "close newdirfd failed");
-
-	if (fd > 0 && close(fd) < 0)
-		tst_resm(TWARN | TERRNO, "close fd failed");
-
-	tst_rmdir();
-
+	if (newdirfd != -1)
+		SAFE_CLOSE(newdirfd);
 }
 
-static void renameat2_verify(void)
-{
-	char str[BUFSIZ] = { 0 };
-	struct stat st;
-	char *emptyfile;
-	char *contentfile;
-	int readn, data_len;
-
-	if (TEST_ERRNO == EINVAL && TST_BTRFS_MAGIC == fs_type) {
-		tst_brkm(TCONF, cleanup,
-			"RENAME_EXCHANGE flag is not implemeted on %s",
-			tst_fs_type_name(fs_type));
-	}
-
-	if (TEST_RETURN != 0) {
-		tst_resm(TFAIL | TTERRNO, "renameat2() failed unexpectedly");
-		return;
-	}
-
-	if (cnt % 2 == 1) {
-		emptyfile = TEST_DIR TEST_FILE;
-		contentfile = TEST_DIR2 TEST_FILE2;
-	} else {
-		emptyfile = TEST_DIR2 TEST_FILE2;
-		contentfile = TEST_DIR TEST_FILE;
-	}
-
-	fd = SAFE_OPEN(cleanup, contentfile, O_RDONLY);
-
-	SAFE_STAT(cleanup, emptyfile, &st);
-
-	readn = SAFE_READ(cleanup, 0, fd, str, BUFSIZ);
-
-	if (close(fd) < 0)
-		tst_brkm(TERRNO | TFAIL, cleanup, "close fd failed");
-	fd = 0;
-
-	data_len = sizeof(content) - 1;
-	if (readn != data_len) {
-		tst_resm(TFAIL, "Wrong number of bytes read after renameat2(). "
-				"Expect %d, got %d", data_len, readn);
-		return;
-	}
-	if (strncmp(content, str, data_len)) {
-		tst_resm(TFAIL, "File content changed after renameat2(). "
-				"Expect '%s', got '%s'", content, str);
-		return;
-	}
-	if (st.st_size) {
-		tst_resm(TFAIL, "emptyfile has non-zero file size");
-		return;
-	}
-	tst_resm(TPASS, "renameat2() test passed");
-}
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.cleanup = cleanup,
+	.needs_tmpdir = 1,
+	.skip_filesystems = (const char *const[]) {
+		"btrfs",
+		NULL,
+	},
+};
