e61190b0e19b4d017f1200cf210eae9f323a0b82
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:14 +0200

    syscalls/removexattr01: Convert to new API
    Convert removexattr01 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/removexattr/removexattr01.c
+++ testcases/kernel/syscalls/removexattr/removexattr01.c
@@ -1,124 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
-* Copyright (c) 2016 Fujitsu Ltd.
-* Author: Xiao Yang <yangx.jy@cn.fujitsu.com>
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of version 2 of the GNU General Public License as
-* published by the Free Software Foundation.
-*
-* This program is distributed in the hope that it would be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*
-* You should have received a copy of the GNU General Public License
-* alone with this program.
-*/
+ * Copyright (c) 2016 Fujitsu Ltd.
+ * Author: Xiao Yang <yangx.jy@cn.fujitsu.com>
+ */
 
-/*
-* Test Name: removexattr01
-*
-* Description:
-* The testcase checks the basic functionality of the removexattr(2).
-* removexattr(2) removes the extended attribute identified by
-* name and associated with the given path in the filesystem.
-*/
+/*\
+ * Verify that :manpage:`removexattr(2)` removes an extended attribute
+ * from a file and a subsequent :manpage:`getxattr(2)` returns ENODATA.
+ */
 
 #include "config.h"
-#include <errno.h>
 #include <sys/types.h>
-
 #ifdef HAVE_SYS_XATTR_H
 # include <sys/xattr.h>
 #endif
-
-#include "test.h"
-#include "tso_safe_macros.h"
-
-char *TCID = "removexattr01";
+#include "tst_test.h"
 
 #ifdef HAVE_SYS_XATTR_H
+
 #define USER_KEY	"user.test"
-#define VALUE	"test"
+#define VALUE		"test"
 #define VALUE_SIZE	(sizeof(VALUE) - 1)
-
-static void verify_removexattr(void);
-static void setup(void);
-static void cleanup(void);
-
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		verify_removexattr();
-	}
-
-	cleanup();
-	tst_exit();
-}
 
 static void verify_removexattr(void)
 {
 	int n;
-	int size = 64;
-	char buf[size];
+	char buf[64];
 
 	n = setxattr("testfile", USER_KEY, VALUE, VALUE_SIZE, XATTR_CREATE);
 	if (n == -1) {
-		if (errno == ENOTSUP) {
-			tst_brkm(TCONF, cleanup, "no xattr support in fs or "
-				 "mounted without user_xattr option");
-		} else {
-			tst_brkm(TFAIL | TERRNO, cleanup, "setxattr() failed");
-		}
+		if (errno == ENOTSUP)
+			tst_brk(TCONF, "no xattr support in fs or mounted without user_xattr option");
+		else
+			tst_brk(TBROK | TERRNO, "setxattr() failed");
 	}
 
-	TEST(removexattr("testfile", USER_KEY));
-	if (TEST_RETURN != 0) {
-		tst_resm(TFAIL | TTERRNO, "removexattr() failed");
+	TST_EXP_PASS(removexattr("testfile", USER_KEY));
+	if (!TST_PASS)
 		return;
-	}
 
-	n = getxattr("testfile", USER_KEY, buf, size);
+	n = getxattr("testfile", USER_KEY, buf, sizeof(buf));
 	if (n != -1) {
-		tst_resm(TFAIL, "getxattr() succeeded for deleted key");
+		tst_res(TFAIL, "getxattr() succeeded for deleted key");
 		return;
 	}
 
-	if (errno != ENODATA) {
-		tst_resm(TFAIL | TTERRNO, "getxattr() failed unexpectedly");
-	} else {
-		tst_resm(TPASS, "removexattr() succeeded");
-	}
+	if (errno != ENODATA)
+		tst_res(TFAIL | TERRNO, "getxattr() failed unexpectedly");
+	else
+		tst_res(TPASS, "removexattr() succeeded");
 }
 
 static void setup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	SAFE_TOUCH(cleanup, "testfile", 0644, NULL);
+	SAFE_TOUCH("testfile", 0644, NULL);
 }
 
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.setup = setup,
+	.test_all = verify_removexattr,
+	.needs_tmpdir = 1,
+};
 
 #else /* HAVE_SYS_XATTR_H */
-int main(int ac, char **av)
-{
-	tst_brkm(TCONF, NULL, "<sys/xattr.h> does not exist.");
-}
+TST_TEST_TCONF("<sys/xattr.h> does not exist");
 #endif
