026de50b02cf1df0cb1f9c881fa0733602bf8306
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:14 +0200

    syscalls/mknodat01: Convert to new API
    Convert mknodat01 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/mknodat/mknodat01.c
+++ testcases/kernel/syscalls/mknodat/mknodat01.c
@@ -1,156 +1,93 @@
-/******************************************************************************
- *
- * Copyright (c) International Business Machines  Corp., 2006
- *  Author: Yi Yang <yyangcdl@cn.ibm.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) International Business Machines Corp., 2006
+ * Author: Yi Yang <yyangcdl@cn.ibm.com>
  * Copyright (c) Cyril Hrubis 2014 <chrubis@suse.cz>
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * DESCRIPTION
- *  This test case will verify basic function of mknodat
- *  added by kernel 2.6.16 or up.
- *
- *****************************************************************************/
+ */
+
+/*\
+ * Verify basic :manpage:`mknodat(2)` functionality with various dirfd
+ * and pathname combinations, including success and expected error
+ * conditions (ENOTDIR, EBADF).
+ */
 
 #define _GNU_SOURCE
 
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include "test.h"
-#include "tso_safe_macros.h"
+
+#include "tst_test.h"
+#include "tst_safe_stdio.h"
 #include "lapi/fcntl.h"
 
 #define PATHNAME "mknodattestdir"
 
-static void setup(void);
-static void cleanup(void);
-static void clean(void);
-
-static char testfilepath[256];
-static char testfile[256];
-static char testfile2[256];
-static char testfile3[256];
-
-static int dir_fd, fd;
+static int dir_fd = -1;
+static int fd = -1;
 static int fd_invalid = 100;
 static int fd_atcwd = AT_FDCWD;
 
-static struct test_case {
+static char *abs_path;
+
+static struct tcase {
 	int *dir_fd;
 	const char *name;
-	int exp_ret;
 	int exp_errno;
-} test_cases[] = {
-	{&dir_fd, testfile, 0, 0},
-	{&dir_fd, testfile3, 0, 0},
-	{&fd, testfile2, -1, ENOTDIR},
-	{&fd_invalid, testfile, -1, EBADF},
-	{&fd_atcwd, testfile, 0, 0}
+	const char *desc;
+} tcases[] = {
+	{&dir_fd, "testfile", 0, "relative path with dir fd"},
+	{&dir_fd, NULL, 0, "absolute path with dir fd"},
+	{&fd, "testfile2", ENOTDIR, "non-directory fd"},
+	{&fd_invalid, "testfile", EBADF, "invalid fd"},
+	{&fd_atcwd, "testfile", 0, "AT_FDCWD"},
 };
-
-char *TCID = "mknodat01";
-int TST_TOTAL = ARRAY_SIZE(test_cases);
-
-static dev_t dev;
-
-static void verify_mknodat(struct test_case *test)
-{
-	TEST(mknodat(*test->dir_fd, test->name, S_IFREG, dev));
-
-	if (TEST_RETURN != test->exp_ret) {
-		tst_resm(TFAIL | TTERRNO,
-		         "mknodat() returned %ld, expected %d",
-			 TEST_RETURN, test->exp_ret);
-		return;
-	}
-
-	if (TEST_ERRNO != test->exp_errno) {
-		tst_resm(TFAIL | TTERRNO,
-		         "mknodat() returned wrong errno, expected %d",
-			 test->exp_errno);
-		return;
-	}
-
-	tst_resm(TPASS | TTERRNO, "mknodat() returned %ld", TEST_RETURN);
-}
-
-int main(int ac, char **av)
-{
-	int lc;
-	int i;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++)
-			verify_mknodat(test_cases + i);
-
-		/* clean created nodes before next run */
-		clean();
-	}
-
-	cleanup();
-	tst_exit();
-}
 
 static void setup(void)
 {
-	char *tmpdir;
+	SAFE_MKDIR(PATHNAME, 0700);
 
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+	dir_fd = SAFE_OPEN(PATHNAME, O_DIRECTORY);
+	fd = SAFE_OPEN("testfile2", O_CREAT | O_RDWR, 0600);
 
-	TEST_PAUSE;
+	SAFE_ASPRINTF(&abs_path, "%s/" PATHNAME "/testfile3", tst_tmpdir_path());
 
-	tst_tmpdir();
-
-	/* Initialize test dir and file names */
-	tmpdir = tst_get_tmpdir();
-	sprintf(testfilepath, PATHNAME"/mknodattestfile%d", getpid());
-	sprintf(testfile, "mknodattestfile%d", getpid());
-	sprintf(testfile2, "mknodattestfile2%d", getpid());
-	sprintf(testfile3, "%s/mknodattestfile3%d", tmpdir, getpid());
-	free(tmpdir);
-
-	SAFE_MKDIR(cleanup, PATHNAME, 0700);
-
-	dir_fd = SAFE_OPEN(cleanup, PATHNAME, O_DIRECTORY);
-	fd = SAFE_OPEN(cleanup, testfile2, O_CREAT | O_RDWR, 0600);
+	tcases[1].name = abs_path;
 }
 
-static void clean(void)
+static void run(unsigned int i)
 {
-	SAFE_UNLINK(cleanup, testfilepath);
-	SAFE_UNLINK(cleanup, testfile3);
-	SAFE_UNLINK(cleanup, testfile);
+	struct tcase *tc = &tcases[i];
+
+	if (tc->exp_errno) {
+		TST_EXP_FAIL(mknodat(*tc->dir_fd, tc->name, S_IFREG, 0),
+				tc->exp_errno, "%s", tc->desc);
+	} else {
+		TST_EXP_PASS(mknodat(*tc->dir_fd, tc->name, S_IFREG, 0),
+				"%s", tc->desc);
+	}
+
+	if (TST_PASS && !tc->exp_errno) {
+		if (tc->dir_fd == &dir_fd && tc->name != abs_path)
+			SAFE_UNLINK(PATHNAME "/testfile");
+		else if (tc->name == abs_path)
+			SAFE_UNLINK(abs_path);
+		else
+			SAFE_UNLINK("testfile");
+	}
 }
 
 static void cleanup(void)
 {
-	if (dir_fd > 0 && close(dir_fd))
-		tst_resm(TWARN | TERRNO, "Failed to close(dir_fd)");
+	if (fd != -1)
+		SAFE_CLOSE(fd);
 
-	if (fd > 0 && close(fd))
-		tst_resm(TWARN | TERRNO, "Failed to close(fd)");
-
-	tst_rmdir();
+	if (dir_fd != -1)
+		SAFE_CLOSE(dir_fd);
 }
+
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
+	.setup = setup,
+	.cleanup = cleanup,
+	.needs_tmpdir = 1,
+};
