6d867b21692a070e2943ad5255dde6d8be265e5e
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:13 +0200

    syscalls/fstatat01: Convert to new API
    Convert fstatat01 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/fstatat/fstatat01.c
+++ testcases/kernel/syscalls/fstatat/fstatat01.c
@@ -1,152 +1,98 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2016 Oracle and/or its affiliates. All Rights Reserved.
- * Copyright (c) International Business Machines  Corp., 2006
+ * Copyright (c) International Business Machines Corp., 2006
+ * Author: Yi Yang <yyangcdl@cn.ibm.com>
+ */
+
+/*\
+ * Verify that :manpage:`fstatat(2)` handles various dirfd and flag
+ * combinations correctly:
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * DESCRIPTION
- *	This test case will verify basic function of fstatat64/newfstatat
- *	added by kernel 2.6.16 or up.
- *
- * Author
- *	Yi Yang <yyangcdl@cn.ibm.com>
+ * - Valid directory fd with relative path succeeds
+ * - Valid directory fd with absolute path succeeds
+ * - Regular file fd as dirfd fails with ENOTDIR
+ * - Invalid fd fails with EBADF
+ * - Invalid flags value fails with EINVAL
+ * - AT_FDCWD with relative path succeeds
  */
 
 #define _GNU_SOURCE
-#include <sys/types.h>
+
 #include <sys/stat.h>
-#include <sys/time.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include "config.h"
-#include "test.h"
-#include "tso_safe_macros.h"
-#include "lapi/syscalls.h"
 
-#define TEST_CASES 6
-#ifndef AT_FDCWD
-#define AT_FDCWD -100
-#endif
+#include "tst_test.h"
+#include "tst_safe_stdio.h"
+#include "lapi/fcntl.h"
 
-void setup();
-void cleanup();
+#define TESTDIR		"fstatattestdir"
+#define TESTFILE	"fstatattestfile.txt"
+#define TESTFILE2	TESTDIR "/fstatattestfile.txt"
 
-char *TCID = "fstatat01";
-int TST_TOTAL = TEST_CASES;
+static int dir_fd = -1;
+static int file_fd = -1;
+static int bad_fd = 100;
+static int atcwd_fd = AT_FDCWD;
 
-static const char pathname[] = "fstatattestdir",
-		  testfile[] = "fstatattestfile.txt",
-		  testfile2[] = "fstatattestdir/fstatattestfile.txt";
-static char *testfile3;
+static char *abs_path;
 
-static int fds[TEST_CASES];
-static const char *filenames[TEST_CASES];
-static const int expected_errno[] = { 0, 0, ENOTDIR, EBADF, EINVAL, 0 };
-static const int flags[] = { 0, 0, 0, 0, 9999, 0 };
+static struct tcase {
+	int *fd;
+	const char **path;
+	int flags;
+	int exp_errno;
+	const char *desc;
+} tcases[] = {
+	{&dir_fd, &(const char *){TESTFILE}, 0, 0, "dirfd + relative path"},
+	{&dir_fd, NULL, 0, 0, "dirfd + absolute path"},
+	{&file_fd, &(const char *){TESTFILE}, 0, ENOTDIR, "non-dir fd"},
+	{&bad_fd, &(const char *){TESTFILE}, 0, EBADF, "bad fd"},
+	{&dir_fd, &(const char *){TESTFILE}, 9999, EINVAL, "invalid flags"},
+	{&atcwd_fd, &(const char *){TESTFILE}, 0, 0, "AT_FDCWD"},
+};
 
-#if !defined(HAVE_FSTATAT)
-#if (__NR_fstatat64 > 0)
-int fstatat(int dirfd, const char *filename, struct stat64 *statbuf, int flags)
+static void setup(void)
 {
-	return tst_syscall(__NR_fstatat64, dirfd, filename, statbuf, flags);
+	SAFE_MKDIR(TESTDIR, 0700);
+	dir_fd = SAFE_OPEN(TESTDIR, O_DIRECTORY);
+
+	SAFE_FILE_PRINTF(TESTFILE, TESTFILE);
+	SAFE_FILE_PRINTF(TESTFILE2, TESTFILE2);
+
+	SAFE_ASPRINTF(&abs_path, "%s/fstatattestfile3.txt", tst_tmpdir_path());
+	file_fd = SAFE_OPEN(abs_path, O_CREAT | O_RDWR, 0600);
+
+	tcases[1].path = (const char **)&abs_path;
 }
-#elif (__NR_newfstatat > 0)
-int fstatat(int dirfd, const char *filename, struct stat *statbuf, int flags)
+
+static void run(unsigned int i)
 {
-	return tst_syscall(__NR_newfstatat, dirfd, filename, statbuf, flags);
-}
-#else
-int fstatat(int dirfd, const char *filename, struct stat *statbuf, int flags)
-{
-	return tst_syscall(__NR_fstatat, dirfd, filename, statbuf, flags);
-}
-#endif
-#endif
+	struct tcase *tc = &tcases[i];
+	struct stat statbuf;
+	const char *path = tc->path ? *tc->path : TESTFILE;
 
-int main(int ac, char **av)
-{
-	int lc, i;
-#if !defined(HAVE_FSTATAT) && (__NR_fstatat64 > 0)
-	static struct stat64 statbuf;
-#else
-	static struct stat statbuf;
-#endif
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++) {
-			TEST(fstatat(fds[i], filenames[i], &statbuf, flags[i]));
-
-			if (TEST_ERRNO == expected_errno[i]) {
-				tst_resm(TPASS | TTERRNO,
-					 "fstatat failed as expected");
-			} else
-				tst_resm(TFAIL | TTERRNO, "fstatat failed");
-		}
-
+	if (tc->exp_errno) {
+		TST_EXP_FAIL(fstatat(*tc->fd, path, &statbuf, tc->flags),
+				tc->exp_errno, "%s", tc->desc);
+	} else {
+		TST_EXP_PASS(fstatat(*tc->fd, path, &statbuf, tc->flags),
+				"%s", tc->desc);
 	}
-
-	cleanup();
-	tst_exit();
 }
 
-void setup(void)
+static void cleanup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+	if (file_fd != -1)
+		SAFE_CLOSE(file_fd);
 
-	tst_tmpdir();
-
-	char *abs_path = tst_get_tmpdir();
-
-	SAFE_ASPRINTF(cleanup, &testfile3, "%s/fstatattestfile3.txt", abs_path);
-	free(abs_path);
-
-	SAFE_MKDIR(cleanup, pathname, 0700);
-
-	fds[0] = SAFE_OPEN(cleanup, pathname, O_DIRECTORY);
-	fds[1] = fds[4] = fds[0];
-
-	SAFE_FILE_PRINTF(cleanup, testfile, testfile);
-	SAFE_FILE_PRINTF(cleanup, testfile2, testfile2);
-
-	fds[2] =  SAFE_OPEN(cleanup, testfile3, O_CREAT | O_RDWR, 0600);
-
-	fds[3] = 100;
-	fds[5] = AT_FDCWD;
-
-	filenames[0] = filenames[2] = filenames[3] = filenames[4] =
-	    filenames[5] = testfile;
-	filenames[1] = testfile3;
-
-	TEST_PAUSE;
+	if (dir_fd != -1)
+		SAFE_CLOSE(dir_fd);
 }
 
-void cleanup(void)
-{
-	if (fds[0] > 0)
-		close(fds[0]);
-	if (fds[2] > 0)
-		close(fds[2]);
-
-	free(testfile3);
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
+	.setup = setup,
+	.cleanup = cleanup,
+	.needs_tmpdir = 1,
+};
