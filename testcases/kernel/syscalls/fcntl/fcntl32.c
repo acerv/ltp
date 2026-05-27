488d99860009c40a4aa38fcf9edd5a3d80f8db38
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-27 10:46:13 +0200

    syscalls/fcntl32: Convert to new API
    Convert fcntl32 test to the new LTP API.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/syscalls/fcntl/fcntl32.c
+++ testcases/kernel/syscalls/fcntl/fcntl32.c
@@ -1,40 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2015 Fujitsu Ltd.
  * Author: Guangwen Feng <fenggw-fnst@cn.fujitsu.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License
- * alone with this program.
  */
 
-/*
- * DESCRIPTION
- *  Basic test for fcntl(2) using F_SETLEASE & F_WRLCK argument.
- *  "A write lease may be placed on a file only if there are
- *  no other open file descriptors for the file."
+/*\
+ * Verify that :manpage:`fcntl(2)` with F_SETLEASE and F_WRLCK fails with
+ * EAGAIN or EBUSY when another open file descriptor exists for the file.
+ *
+ * A write lease may be placed on a file only if there are no other open
+ * file descriptors for the file.
  */
 
 #include <errno.h>
 
-#include "test.h"
-#include "tso_safe_macros.h"
+#include "tst_test.h"
 
-static void setup(void);
-static void verify_fcntl(int);
-static void cleanup(void);
+#define FILE_MODE	0777
 
-#define FILE_MODE	(S_IRWXU | S_IRWXG | S_IRWXO | S_ISUID | S_ISGID)
-
-static int fd1;
-static int fd2;
-static long type;
+static int fd1 = -1;
+static int fd2 = -1;
 
 static struct test_case_t {
 	int fd1_flag;
@@ -51,87 +36,45 @@ static struct test_case_t {
 	{O_RDWR, O_RDWR},
 };
 
-char *TCID = "fcntl32";
-int TST_TOTAL = ARRAY_SIZE(test_cases);
-
-int main(int ac, char **av)
-{
-	int lc;
-	int tc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		for (tc = 0; tc < TST_TOTAL; tc++)
-			verify_fcntl(tc);
-	}
-
-	cleanup();
-	tst_exit();
-}
-
 static void setup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	switch ((type = tst_fs_type(cleanup, "."))) {
-	case TST_NFS_MAGIC:
-	case TST_RAMFS_MAGIC:
-	case TST_TMPFS_MAGIC:
-		tst_brkm(TCONF, cleanup,
-			 "Cannot do fcntl(F_SETLEASE, F_WRLCK) "
-			 "on %s filesystem",
-			 tst_fs_type_name(type));
-	default:
-		break;
-	}
-
-	SAFE_TOUCH(cleanup, "file", FILE_MODE, NULL);
+	SAFE_TOUCH("file", FILE_MODE, NULL);
 }
 
-static void verify_fcntl(int i)
+static void verify_fcntl(unsigned int i)
 {
-	fd1 = SAFE_OPEN(cleanup, "file", test_cases[i].fd1_flag);
-	fd2 = SAFE_OPEN(cleanup, "file", test_cases[i].fd2_flag);
+	fd1 = SAFE_OPEN("file", test_cases[i].fd1_flag);
+	fd2 = SAFE_OPEN("file", test_cases[i].fd2_flag);
 
-	TEST(fcntl(fd1, F_SETLEASE, F_WRLCK));
+	static const int exp_errs[] = {EAGAIN, EBUSY};
 
-	if (TEST_RETURN == 0) {
-		tst_resm(TFAIL, "fcntl(F_SETLEASE, F_WRLCK) "
-			 "succeeded unexpectedly");
-	} else {
-		if (TEST_ERRNO == EBUSY || TEST_ERRNO == EAGAIN) {
-			tst_resm(TPASS | TTERRNO,
-				 "fcntl(F_SETLEASE, F_WRLCK) "
-				 "failed as expected");
-		} else {
-			tst_resm(TFAIL | TTERRNO,
-				 "fcntl(F_SETLEASE, F_WRLCK) "
-				 "failed unexpectedly, "
-				 "expected errno is EBUSY or EAGAIN");
-		}
-	}
+	TST_EXP_FAIL_ARR(fcntl(fd1, F_SETLEASE, F_WRLCK),
+			exp_errs, ARRAY_SIZE(exp_errs),
+			"fcntl(F_SETLEASE, F_WRLCK)");
 
-	SAFE_CLOSE(cleanup, fd1);
-	fd1 = 0;
-	SAFE_CLOSE(cleanup, fd2);
-	fd2 = 0;
+	SAFE_CLOSE(fd1);
+	SAFE_CLOSE(fd2);
 }
 
 static void cleanup(void)
 {
-	if (fd1 > 0 && close(fd1))
-		tst_resm(TWARN | TERRNO, "Failed to close file");
+	if (fd1 != -1)
+		SAFE_CLOSE(fd1);
 
-	if (fd2 > 0 && close(fd2))
-		tst_resm(TWARN | TERRNO, "Failed to close file");
-
-	tst_rmdir();
+	if (fd2 != -1)
+		SAFE_CLOSE(fd2);
 }
+
+static struct tst_test test = {
+	.setup = setup,
+	.test = verify_fcntl,
+	.tcnt = ARRAY_SIZE(test_cases),
+	.cleanup = cleanup,
+	.needs_tmpdir = 1,
+	.skip_filesystems = (const char *const []) {
+		"nfs",
+		"ramfs",
+		"tmpfs",
+		NULL
+	},
+};
